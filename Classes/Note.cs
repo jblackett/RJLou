6a889ccb8af7f0a553a5b5a03347350f344628b5
Joseph Blackett <joseph.blackett@gmail.com>
2014-02-15 17:19:55 -0500

    created Note class and empty InternalUser class

---
+++ Classes/Note.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RJLou.Classes
+{
+    public class Note
+    {
+        #region Private Variables
+        private int _noteID;
+        private DateTime _createDate;
+        private DateTime _editDate;
+        private InternalUser _author;
+        private string _noteText;
+        #endregion
+
+        #region Public Properties
+        public int NoteID
+        {
+            get
+            {
+                return _noteID;
+            }
+            set
+            {
+                _noteID = value;
+            }
+        }
+
+        public DateTime CreateDate
+        {
+            get
+            {
+                return _createDate;
+            }
+            set
+            {
+                _createDate = value;
+            }
+        }
+
+        public DateTime EditDate
+        {
+            get
+            {
+                return _editDate;
+            }
+            set
+            {
+                _editDate = value;
+            }
+        } 
+
+        public InternalUser Author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                _author = value;
+            }
+        }
+
+        public string NoteText
+        {
+            get
+            {
+                return _noteText;
+            }
+            set
+            {
+                _noteText = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public Note() { }
+
+        public Note(int noteID, DateTime createDate, InternalUser author, string noteText)
+        {
+            NoteID = noteID;
+            CreateDate = createDate;
+            Author = author;
+            NoteText = noteText;
+            EditDate = null;
+        }
+
+        public Note(int noteID, DateTime createDate, InternalUser author, string noteText, DateTime editDate)
+        {
+            NoteID = noteID;
+            CreateDate = createDate;
+            Author = author;
+            NoteText = noteText;
+            EditDate = editDate;
+        }
+        #endregion
+
+    }
+}
