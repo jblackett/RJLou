bfa538ae0281f74fb89f425fe18541ec64572728
Zach Woodward <zachw38@gmail.com>
2014-02-15 17:39:30 -0500

    Random commit that'll be overridden

---
+++ Classes/Donation.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RJLou.Classes
+{
+    public class Donation
+    {
+        #region Private Variables
+        private int _donationID;
+        private int _donorID;
+        private double _amount;
+        private int _eventID;
+        #endregion
+
+        #region Public Properties
+        public int DonationID
+        {
+            get
+            {
+                return _donationID;
+            }
+
+            set
+            {
+                _donationID = value;
+            }
+        }
+
+        public int DonorID
+        {
+            get
+            {
+                return _donorID;
+            }
+
+            set
+            {
+                _donorID = value;
+            }
+        }
+
+        public double Amount
+        {
+            get
+            {
+                return _amount;
+            }
+
+            set
+            {
+                _amount = value;
+            }
+        }
+
+        public int EventID
+        {
+            get
+            {
+                return _eventID;
+            }
+
+            set
+            {
+                _eventID = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public Donation() { }
+        #endregion
+
+        #region Methods
+        public static Donation Get(int id)
+        {
+            string dsn = ConfigurationManager.ConnectionStrings["RJLouEntities"].ToString();
+            string sql = "SELECT * FROM Donation WHERE Donation_ID = @ID";
+
+            using (SqlConnection conn = new SqlConnection(dsn))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("ID", id);
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                if (read.Read())
+                {
+                    Donation result = new Donation()
+                    {
+                        DonationID = Convert.ToInt32(read["Donation_ID"]),
+                        DonorID = Convert.ToInt32(read["Donor_ID"]),
+                        Amount = Convert.ToDouble(read["Amount"]),
+                        EventID = Convert.ToInt32(read["Event_ID"])
+                    };
+
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
+        public static List<Donation> GetDonations()
+        {
+            string sql = "SELECT * FROM Donation";
+            string dsn = ConfigurationManager.ConnectionStrings["RJLouEntities"].ToString();
+            List<Donation> results = new List<Donation>();
+
+            using (SqlConnection conn = new SqlConnection(dsn))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                while (read.Read())
+                {
+                    results.Add(new Donation()
+                        {
+                            DonationID = Convert.ToInt32(read["Donation_ID"]),
+                            DonorID = Convert.ToInt32(read["Donor_ID"]),
+                            Amount = Convert.ToDouble(read["Amount"]),
+                            EventID = Convert.ToInt32(read["Event_ID"])
+                        });
+                }
+            }
+
+            return results;
+        }
+        #endregion
+    }
+}
