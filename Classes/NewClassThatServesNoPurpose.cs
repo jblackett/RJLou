bf4fecd19eb74abbb49a62aca0d3271ce4052643
Joseph Blackett <joseph.blackett@gmail.com>
2014-02-27 21:43:48 -0500

    tested new gitignore, appears to be working

---
+++ Classes/NewClassThatServesNoPurpose.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RJLou.Classes
+{
+    abstract class NewClassThatServesNoPurpose
+    {
+        private int pointlessInt = 0;
+        private string uselessString = "Serves No Puprose";
+
+        public NewClassThatServesNoPurpose(int useless, string pointless) { }
+    }
+}
