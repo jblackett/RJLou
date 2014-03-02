8a3303e45ac7ef5472eb362d11f37b05841a9bcc
annakelley <annakelley4@gmail.com>
2014-03-02 17:28:52 -0500

    contacts

---
+++ Admin/Contacts.aspx
@@ -0,0 +1,205 @@
+﻿<%@ Page MasterPageFile="~/Masterpages/Admin.master" Language="C#" AutoEventWireup="true" CodeBehind="Contacts.aspx.cs" Inherits="RJLou.Default" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+    <asp:ScriptManager runat='server' ID='workingMan' />
+
+    //Not sure if top line is quite right. 
+    //What I'm doing in this and also in Contacts.aspx.cs is to just rename things so they mirror what 
+    //default is basically doing (so instead of Case, I use Person; instead of Cases, I use Persons) and
+    //also to attempt to use those template html pages we first used to imagine things
+
+    <div id="container_left" class="container left">
+        <table class="changes" cellspacing="0" border="0">
+            <tr>
+                <td><asp:LinkButton runat="server" ID="PersonSwitchAll" OnClick="SwitchPersonList" Text="All" CommandArgument="all" /></td>
+                <td><asp:LinkButton runat="server" ID="PersonSwitchEmployees" OnClick="SwitchPersonList" Text="Employees" CommandArgument="employees" /></td>
+                <td><asp:LinkButton runat="server" ID="PersonSwitchOffenders" OnClick="SwitchPersonList" Text="Offenders" CommandArgument="offenders" /></td>
+                <td><asp:LinkButton runat="server" ID="PersonSwitchVictims" OnClick="SwitchPersonList" Text="Victims" CommandArgument="victims" /></td>
+            </tr>
+        </table>
+        <asp:Repeater runat="server" ID="PersonsRepeater" OnItemDataBound="PersonsRepeater_Databind">
+            <HeaderTemplate>
+                <table cellspacing="0" border="0">
+                    <thead>
+                        <tr>
+                            //Again, based it off the html mock ups we came up with. Do we really want to base
+                            //it off Name, Phone, and Address, though? People can have multiple phones and
+                            //addresses. Then what would you do to determine which? I propose Name, and at least their
+                            //type/title (victim, offender, etc). Maybe for a third, consider email? even if they don't
+                            //have one and it's empty for some people, they'll only have one.
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    //the first line under this is probably okay, but the changes I made to the two lines under 
+                    //it I'm really not sure about.
+                    <td><asp:LinkButton runat="server" ID="PersonButton" OnClick="LoadPerson" Text='<%# Eval("PersonID") %>' CommandArgument='<%# Eval("PersonID") %>' /></td>
+                    <td><asp:Label ID="Phone" runat="server"></asp:Label></td>
+                    <td><%#DataBinder.Eval(Container.DataItem, "Address")%></td>
+                </tr>
+            </ItemTemplate>
+            <FooterTemplate>
+                    </tbody>
+                </table>
+            </FooterTemplate>
+        </asp:Repeater>
+    </div>
+    <div class="container right">
+        <asp:UpdatePanel ID="MainContainer" runat="server" Visible="false">
+            <ContentTemplate>
+                <div class="scroll-stick">
+                    //On here I made the option to scroll to Contact and Cases....that's what we have
+                    //on our template. But what about Guardians? What do we do with them? or they on
+                    //a victim or offender contacts page? I'm not sure what to do to resolve this. wouldn't 
+                    //different type of contact have a bit of a different page? employees won't have guardians.
+                    //Only offenders have a special ID Num given by the court that employees might need to know.
+                    //For internal users, do we collect their phone numbers and addresses? Will they need sections for those?
+                    //Another thing to bring up, I see we have a CASE MANAGER table in the database to connect
+                    //them to their cases I guess. What about connecting facilitators to their cases?
+                    <a class="smaller" href="#contact">Contact</a>
+                    <a class="smaller" href="#cases">Cases</a>
+                </div>
+                <div style="clear: both;"></div>
+                <asp:Panel ID="PersonUpdatedPanel" runat="server" CssClass="updatepanel">
+                    <p>
+                        This person was successfully saved!
+                    </p>
+                    <span class="x alert">X</span>
+                </asp:Panel>
+                <h1 id="contact">Contact</h1>
+                <table class="nothing">
+                    //Again, going off the Person tbale. But what about victims and offenders? They'll have guardians.
+                    //Offenders will have their court offender id/num.
+                    <tr>
+                        <td>First Name:</td>
+                        <td><asp:TextBox ID="FirstName" runat="server" /></td>
+                    </tr>
+                    <tr>
+                        <td>Last Name:</td>
+                        <td><asp:TextBox ID="LastName" runat="server" /></td>
+                    </tr>
+                    <tr>
+                        <td>Date of Birth:</td>
+                        <td><asp:TextBox ID="DateOfBirth" runat="server" /></td>
+                    </tr>
+                    <tr>
+                        <td>Gender:</td>
+                        <td><asp:TextBox ID="Gender" runat="server" /></td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><asp:TextBox ID="Email" runat="server" /></td>
+                    </tr>
+                    <tr>
+                        <td>Race:</td>
+                        <td><asp:TextBox ID="Race" runat="server" /></td>
+                    </tr>
+                </table>
+                <asp:LinkButton runat="server" CssClass="button" Text="Save Person" OnClick="SavePerson" />
+                <h1 id="cases">Cases</h1>
+                <div class="inner">
+                    <asp:Repeater ID="CasesRepeater" runat="server" OnItemDataBound="CasesRepeater_ItemDataBound">
+                        <HeaderTemplate>
+                            <table cellspacing="0" border="0">
+                                <thead>
+                                    <tr>
+                                        //On the html template page for this, we use Case ID and then there's a column
+                                        //called Case that just seems to list persons? I'm going to take a guess based on our
+                                        //presentation: I believe she would want a single offender name in that column. She
+                                        //said that when you view a case, you should just see one offender of whose case it is
+                                        //and that for employees identifying the case they probably just see it as that offender's
+                                        //name for the most part in  theirs heads.
+                                        <th>ID</th>
+                                        <th>Offender</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                        </HeaderTemplate>
+                        <ItemTemplate>
+                            <tr>
+                                //I'll be honest, not sure how this is suppose to set up. Left this section for where we
+                                //list cases assigned/involved in, I gues. Going off the html template, we don't need
+                                //to delete or view button. Maybe hotlink the ID so you can go to the case page.
+                                <td><asp:Label ID="VictimName" runat="server" /></td>
+                                <td><%#DataBinder.Eval(Container.DataItem, "Gender")%></td>
+                                <td><%#DataBinder.Eval(Container.DataItem, "Race")%></td>
+                                <td>
+                                    <asp:LinkButton runat="server" ID="VictimDeleteButton" OnClick="DeleteVictim" Text="Delete" CommandArgument='<%# Eval("PersonID") %>' /> &nbsp;
+                                    <asp:LinkButton runat="server" ID="VictimViewButton" OnClick="ViewVictim" Text="View" CommandArgument='<%# Eval("PersonID") %>' />
+                                </td>
+                            </tr>
+                        </ItemTemplate>
+                        <FooterTemplate>
+                                </tbody>
+                            </table>
+                            <a class="button float-right" href="#">Add Victim</a>
+                        </FooterTemplate>
+                    </asp:Repeater>
+                </div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+    <div style="margin: 0; padding: 0; clear: both;"></div>
+    
+    //Again, don't konw if we really need to be able to view and see a modal box, but instead maybe just
+    //hot link to the case page.
+    <asp:Panel ID="ViewPersonModalPanel" runat="server" CssClass="modal-background">
+        <div class="modal">
+            <h1 id="ModalName" runat="server"></h1>
+            <table class="nothing">
+                <tr>
+                    <td>Date of Birth:</td>
+                    <td><asp:TextBox ID="ModalDateOfBirth" runat="server" ReadOnly="true" /></td>
+                </tr>
+                <tr>
+                    <td>Gender:</td>
+                    <td><asp:TextBox ID="ModalGender" runat="server" ReadOnly="true" /></td>
+                </tr>
+                <tr>
+                    <td>Race:</td>
+                    <td><asp:TextBox ID="ModalRace" runat="server" ReadOnly="true" /></td>
+                </tr>
+                <tr>
+                    <td>Phone Numbers:</td>
+                    <td>
+                        <asp:Repeater ID="ModalPhoneNumbers" runat="server" OnItemDataBound="ModalPhoneNumbers_ItemDataBound">
+                            <ItemTemplate>
+                                <asp:TextBox ID="ModalPhoneNum" runat="server" ReadOnly="true" /> <br />
+                            </ItemTemplate>
+                        </asp:Repeater>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Addresses:</td>
+                    <td>
+                        <asp:Repeater ID="ModalAddresses" runat="server" OnItemDataBound="ModalAddresses_ItemDataBound">
+                            <ItemTemplate>
+                                <asp:TextBox ID="ModalAddress" runat="server" ReadOnly="true" Width="300" /> <br />
+                            </ItemTemplate>
+                        </asp:Repeater>
+                    </td>
+                </tr>
+            </table>
+        //Maybe I'm stupid...this button below was originally Edit Case, but I don't see it anywhere on
+        //the case page?
+            <a class="button" href="#">Edit Person</a>
+            <span class="x popup" runat="server">X</span>
+        </div>
+    </asp:Panel>
+    //Not sure what to do about the second one down there since I'm unsure about the modals.
+    //just changed the .removeClass to .removePerson
+    <script type="text/javascript">
+        $('span.x.alert').click(function () {
+            $('.updatepanel').removePerson('visible');
+        });
+
+        $('span.x.popup').click(function () {
+            $('.modal-background').removeClass('visible');
+        })
+    </script>
+</asp:Content>
---
+++ Admin/Contacts.aspx.cs
@@ -0,0 +1,353 @@
+﻿using RJLou.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RJLou
+{
+    public partial class Contacts : System.Web.UI.Page
+    {
+        Person thisPerson;
+        List<Person> persons;
+        int PersonID = -1;
+
+        //Guessing this is for logging in, so i'm not changing anything besides the caes, casesprepeater stuff
+        //However, in our Persons class, we do not have a GetPersons method, so not sure what
+        //you all want to do about that.
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try { PersonID = Convert.ToInt32(Session["PersonID"]); }
+            catch { }
+
+            if (PersonID <= 0)
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+            if (!Page.IsPostBack)
+            {
+                InternalUser thisUser = InternalUser.Get(PersonID);
+                if (thisUser.Role == Role.CASE_MANAGER)
+                {
+                    persons = Person.GetPersons(true, thisUser.PersonID);
+                    PersonsRepeater.DataSource = persons;
+                    PersonsRepeater.DataBind();
+                }
+                else
+                {
+                    persons = Person.GetPersons(true);
+                    PersonsRepeater.DataSource = persons;
+                    PersonsRepeater.DataBind();
+                }
+            }
+        }
+
+        //Have not changed since back in my Contacts.aspx page I didn't do anything with 
+        //this seciton.
+        protected internal void BindData()
+        {
+            VictimsRepeater.DataSource = thisCase.Victims;
+            VictimsRepeater.DataBind();
+
+            OffendersRepeater.DataSource = thisCase.Offenders;
+            OffendersRepeater.DataBind();
+
+            AffiliatesRepeater.DataSource = thisCase.Affiliates;
+            AffiliatesRepeater.DataBind();
+
+            NotesRepeater.DataSource = thisCase.Notes;
+            NotesRepeater.DataBind();
+
+            ChargesRepeater.DataSource = thisCase.Charges;
+            ChargesRepeater.DataBind();
+
+            DocumentsRepeater.DataSource = thisCase.Documents;
+            DocumentsRepeater.DataBind();
+        }
+
+        protected internal void PersonsRepeater_Databind(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                //Guessing we still need this? changed case to person
+                Label thisLabel = (Label)e.Item.FindControl("Name");
+                Person currentPerson = (Person)e.Item.DataItem;
+
+                string Name;
+
+                //is this wehre we would bind phonenumbers or addresses?
+                if (currentCase.Offenders.Count > 0)
+                    Name = currentCase.Offenders[0].FirstName + " " + currentCase.Offenders[0].LastName;
+                else if (currentCase.Victims.Count > 0)
+                    Name = currentCase.Victims[0].FirstName + " " + currentCase.Victims[0].LastName;
+                else
+                    Name = "";
+
+                thisLabel.Text = Name;
+            }
+        }
+
+        protected void VictimsRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Label victimName = (Label)e.Item.FindControl("VictimName");
+                Victim currentVictim = (Victim)e.Item.DataItem;
+
+                string name = currentVictim.FirstName + " " + currentVictim.LastName;
+
+                victimName.Text = name;
+            }
+        }
+
+        protected void OffendersRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Label offenderName = (Label)e.Item.FindControl("OffenderName");
+                Offender currentOffender = (Offender)e.Item.DataItem;
+
+                string name = currentOffender.FirstName + " " + currentOffender.LastName;
+
+                offenderName.Text = name;
+            }
+        }
+
+        protected void AffiliatesRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Label affiliateName = (Label)e.Item.FindControl("AffiliateName");
+                Affiliate currentAffiliate = (Affiliate)e.Item.DataItem;
+
+                string name = currentAffiliate.FirstName + " " + currentAffiliate.LastName;
+
+                affiliateName.Text = name;
+            }
+        }
+
+        /* Not relevant
+        protected void NotesRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Label NoteAuthorAndDate = (Label)e.Item.FindControl("NoteAuthorAndDate");
+                TextBox NoteText = (TextBox)e.Item.FindControl("NoteText");
+                Note currentNote = (Note)e.Item.DataItem;
+
+                string text = "";
+
+                if (currentNote.Author != null)
+                {
+                    text += currentNote.Author.FirstName + " " + currentNote.Author.LastName;
+                }
+                else
+                {
+                    text += "[[NO AUTHOR]]";
+                }
+
+                if (!string.IsNullOrEmpty(currentNote.EditDate.ToString()) && currentNote.EditDate != default(DateTime))
+                {
+                    text += " - " + ((DateTime)currentNote.EditDate).ToString("MM/dd/yyyy");
+                }
+                else if (!string.IsNullOrEmpty(currentNote.CreateDate.ToString()) && currentNote.CreateDate != default(DateTime))
+                {
+                    text += " - " + currentNote.CreateDate.ToString("MM/dd/yyyy");
+                }
+
+                NoteAuthorAndDate.Text = text;
+
+                NoteText.Text = currentNote.NoteText;
+            }
+        }
+
+        protected void ChargesRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+        }
+
+        protected void DocumentsRepeater_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+        }
+         */
+
+        protected internal void LoadPerson(object sender, EventArgs e)
+        {
+            //hmm...used lowercase personID because I believe we were using uppercase
+            //PersonID for the logged in user, correct?
+            int personID = -1;
+            int.TryParse(((LinkButton)sender).CommandArgument, out personID);
+
+            thisPerson = Person.Get(PersonID);
+
+            BindData();
+            LoadHeader();
+        }
+
+        protected internal void LoadHeader()
+        {
+            FirstName.Text = thisPerson.FirstName.ToString();
+            LastName.Text = thisPerson.LastName.ToString();
+            DateOfBirth.Text = (thisPerson.DateOfBirth == default(DateTime) ? "" : thisPerson.DateOfBirth.ToString("MM/dd/yyyy"));
+            Gender.Text = thisPerson.Gender.ToString();
+            Email.Text = thisPerson.Email.ToString();
+            Race.Text = thisPerson.Race.ToString();
+
+            MainContainer.Visible = true;
+        }
+
+        protected internal void SaveCase(object sender, EventArgs e)
+        {
+            //when to use personID vs PersonID since PersonID at the top is also used for logging in?
+            int personID = int.Parse(PersonID.Text);
+            thisPerson = Person.Get(PersonID);
+
+
+
+            thisPerson.PersonID = Convert.ToInt32(PersonID.Text);
+
+            thisPerson.FirstName = FirstName.Text;
+            thisPerson.LastName = LastName.Text;
+            thisPerson.DateOfBirth = Convert.ToDateTime(ReferralDate.Text);
+            thisPerson.Gender = Gender.Text;
+            thisPerson.Email = Email.Text;
+            thisPerson.Race = Race.Text;
+
+            thisPerson.Update();
+            PersonUpdatedPanel.CssClass += " visible";
+        }
+
+        /*
+        protected internal void DeleteVictim(object sender, EventArgs e)
+        {
+
+        }
+
+        protected internal void ViewVictim(object sender, EventArgs e)
+        {
+            int PersonID = Convert.ToInt32(((LinkButton)sender).CommandArgument);
+            Victim thisVictim = Victim.Get(PersonID);
+
+            ModalName.InnerText = thisVictim.FirstName + " " + thisVictim.LastName;
+            ModalDateOfBirth.Text = thisVictim.DateOfBirth.ToString("MM/dd/yyyy");
+            ModalGender.Text = thisVictim.Gender;
+            ModalRace.Text = thisVictim.Race;
+            ModalPhoneNumbers.DataSource = thisVictim.PhoneNumbers;
+            ModalPhoneNumbers.DataBind();
+            ModalAddresses.DataSource = thisVictim.Addresses;
+            ModalAddresses.DataBind();
+
+            ViewPersonModalPanel.CssClass += " visible";
+        }
+
+        protected void ModalPhoneNumbers_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                PhoneNumber thisNumber = (PhoneNumber)e.Item.DataItem;
+                TextBox ModalPhoneNum = (TextBox)e.Item.FindControl("ModalPhoneNum");
+
+                ModalPhoneNum.Text = thisNumber.ToString();
+            }
+        }
+
+        protected void ModalAddresses_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Address thisAddress = (Address)e.Item.DataItem;
+                TextBox ModalAddress = (TextBox)e.Item.FindControl("ModalAddress");
+
+                ModalAddress.Text = thisAddress.ToString();
+            }
+        }
+
+        protected internal void DeleteOffender(object sender, EventArgs e)
+        {
+
+        }
+
+        protected internal void ViewOffender(object sender, EventArgs e)
+        {
+            int PersonID = Convert.ToInt32(((LinkButton)sender).CommandArgument);
+            Offender thisOffender = Offender.Get(PersonID);
+
+            ModalName.InnerText = thisOffender.FirstName + " " + thisOffender.LastName;
+            ModalDateOfBirth.Text = thisOffender.DateOfBirth.ToString("MM/dd/yyyy");
+            ModalGender.Text = thisOffender.Gender;
+            ModalRace.Text = thisOffender.Race;
+            ModalPhoneNumbers.DataSource = thisOffender.PhoneNumbers;
+            ModalPhoneNumbers.DataBind();
+            ModalAddresses.DataSource = thisOffender.Addresses;
+            ModalAddresses.DataBind();
+
+            ViewPersonModalPanel.CssClass += " visible";
+        }
+
+        protected internal void DeleteAffiliate(object sender, EventArgs e)
+        {
+
+        }
+
+        protected internal void ViewAffiliate(object sender, EventArgs e)
+        {
+            int PersonID = Convert.ToInt32(((LinkButton)sender).CommandArgument);
+            Affiliate thisAffiliate = Affiliate.Get(PersonID);
+
+            ModalName.InnerText = thisAffiliate.FirstName + " " + thisAffiliate.LastName;
+            ModalDateOfBirth.Text = thisAffiliate.DateOfBirth.ToString("MM/dd/yyyy");
+            ModalGender.Text = thisAffiliate.Gender;
+            ModalRace.Text = thisAffiliate.Race;
+            ModalPhoneNumbers.DataSource = thisAffiliate.PhoneNumbers;
+            ModalPhoneNumbers.DataBind();
+            ModalAddresses.DataSource = thisAffiliate.Addresses;
+            ModalAddresses.DataBind();
+
+            ViewPersonModalPanel.CssClass += " visible";
+        }
+
+        protected internal void DeleteCharge(object sender, EventArgs e)
+        {
+
+        }
+
+        protected internal void ViewCharge(object sender, EventArgs e)
+        {
+
+        }
+         */
+
+        protected internal void SwitchPersonList(object sender, EventArgs e)
+        {
+            string commandArg = (((LinkButton)sender).CommandArgument).ToString();
+
+            switch (commandArg)
+            {
+                case "all":
+                    persons = Person.GetPersons();
+                    PersonsRepeater.DataSource = persons;
+                    PersonsRepeater.DataBind();
+                    break;
+                //Right now we have the other two columns as phone numbers and addresses,
+                //In my notes in the other page, I think maybe do a title (victim, offender, case manager, etc)
+                //and possible email instead.
+                case "open":
+                case "pending":
+                case "closed":
+                    cases = Case.GetCases(commandArg);
+                    CasesRepeater.DataSource = cases;
+                    CasesRepeater.DataBind();
+                    break;
+                default:
+                    cases = Case.GetCases();
+                    CasesRepeater.DataSource = cases;
+                    CasesRepeater.DataBind();
+                    break;
+            }
+        }
+    }
+}
