15ca2c0e612bfd3bd99a3f09a4943300daee01b5
edgartan <edgartan.uk@gmail.com>
2014-03-29 19:12:57 -0400

    added search page
    search page still works only partial, will display results into a
    gridview table, but has to search with sql query.
    
    Although found stored procedure to search all tables, just need to find
    away to display all the tables in grid view.

---
+++ Admin/Search.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="RJLou.Admin.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+ <form id="form1" runat="server">
+    <div>  
+   <table>
+    <tr>
+    <td> 
+       Search
+        </td>
+        <td>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </td>
+        <td> 
+        <asp:Button ID="Button1" runat="server" Text="Go" OnClick="search1" />
+        </td>
+        
+        </tr>
+ 
+</table>
+<table><tr><td><p><asp:Label ID="Label2" runat="server" Text=""></asp:Label>  </p></td></tr></table>
+ 
+<asp:GridView ID="GridView1" runat="server" >
+    </asp:GridView> 
+    </div>
+ </form>
+</body>
+</html>
---
+++ Admin/Search.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RJLou.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void search1(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Constants.DSN))
+            {
+                conn.Open();
+
+                //string query = "select * from Address_List where Street_Address like'%" + TextBox1.Text + "%'";
+                string query = "Exec SP_SearchTables @tablenames ='%',@searchstr='%" + TextBox1.Text + "%'";
+                SqlDataAdapter da= new SqlDataAdapter(query, conn);
+                DataSet ds= new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                GridView1.Visible = true;
+
+
+                //ds.Read();
+                //rep_bind();
+                //GridView1.Visible = true;
+                //TextBox1.Text = "";
+                //Label2.Text="";
+                
+            }
+        }
+    }
+
+}
