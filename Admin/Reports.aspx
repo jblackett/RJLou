f08fb1d398f97791685c2017f680e4deec62f101
Ptnase01 <ptnase01@gmail.com>
2014-03-30 15:02:09 -0400

    Reports?
    Buttons should send their respective queries (I think), NO idea on how
    to get the results to return....

---
+++ Admin/Reports.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page MasterPageFile="~/Masterpages/Admin.master" Language="C#" AutoEventWireup="true" CodeBehind="Reports.aspx.cs" Inherits="RJLou.Reports" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:ScriptManager runat='server' ID='workingMan' />
+    <div class="container left">
+        <asp:LinkButton ID="Button1"  runat="server" OnClick="Report1_Click" Text="Case By Status"></asp:LinkButton>
+        <asp:LinkButton ID="Button2" runat="server" OnClick="Report2_Click" Text="Case By Offense"></asp:LinkButton>
+        <asp:LinkButton ID="Button3" runat="server" OnClick="Report3_Click" Text="Case By Ethnicity"></asp:LinkButton>
+        <asp:LinkButton ID="Button4" runat="server" OnClick="Report4_Click" Text="Case By Gender"></asp:LinkButton>
+        <asp:LinkButton ID="Button5" runat="server" OnClick="Report5_Click" Text="Case By Age"></asp:LinkButton>
+    </div>
+    <div class="container right">
+        <asp:TextBox ID="TestBox1" runat="server" />
+        <asp:TextBox ID="TestBox2" runat="server" />
+        <table cellspacing="0" border="0">
+            <thead>
+                <tr>
+                    <th>Status</th>
+                    <th>CaseID</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</asp:Content>
---
+++ Admin/Reports.aspx.cs
@@ -0,0 +1,149 @@
+﻿using RJLou.Classes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RJLou.Admin
+{
+    public partial class Reports : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Report1_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Status,Case_ID FROM RJLCase ORDER BY Status WHERE Status = @Status AND Case_ID = @CaseID";
+
+            using (SqlConnection conn = new SqlConnection(Constants.DSN))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("Status", status);
+                cmd.Parameters.AddWithValue("CaseID", caseID);
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                if(read.Read())
+                {
+                    Case result = new Case()
+                    {
+                        Status = read["Status"].ToString(),
+                        CaseID = Convert.ToInt32(read["Case_ID"])
+                    };                    
+                }
+            }
+        }
+
+        protected void Report2_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Description, a.Case_ID, Status FROM RJL_CASE a JOIN CASE_CHARGE b ON a.Case_ID = b.Case_ID JOIN CHARGE c ON c.Charge_ID = b.Charge_ID ORDER BY Description, a.Case_IDWHERE Description = @Description AND Case_ID = @CaseID AND Status = @Status";
+
+            using (SqlConnection conn = new SqlConnection(Constants.DSN))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("Status", status);
+                cmd.Parameters.AddWithValue("CaseID", caseID);
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                if (read.Read())
+                {
+                    Case result = new Case()
+                    {
+                        Status = read["Status"].ToString(),
+                        CaseID = Convert.ToInt32(read["Case_ID"])
+                    };
+                }
+            }
+        }
+
+        protected void Report3_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Race, Status, a.Case_ID FROM RJL_CASE a JOIN CASE_FILE b ON a.Case_ID = b.Case_ID JOIN PERSON c ON c.Person_ID = b.Person_ID JOIN OFFENDER d ON d.Person_ID = c.Person_ID ORDER BY Race WHERE Race = @Race AND CaseID = @CaseID AND Status = @Status"
+
+            using (SqlConnection conn = new SqlConnection(Constants.DSN))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("Race", race);
+                cmd.Parameters.AddWithValue("CaseID", caseID);
+                cmd.Parameters.AddWithValue("Status", status);
+                
+                SqlDataReader read = cmd.ExecuteReader();
+
+                if (read.Read())
+                {
+                
+                }
+            }
+        }
+
+        protected void Report4_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Gender, Status, a.Case_ID FROM RJL_CASE a JOIN CASE_FILE b ON a.Case_ID = b.Case_ID JOIN PERSON c ON c.Person_ID = b.Person_ID JOIN OFFENDER d ON d.Person_ID = c.Person_ID ORDER BY GenderWHERE Gender = @Gender AND Case_ID = @CaseID AND Status = @Status";
+
+            using (SqlConnection conn = new SqlConnection(Constants.DSN))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("Status", status);
+                cmd.Parameters.AddWithValue("CaseID", caseID);
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                if (read.Read())
+                {
+                    Case result = new Case()
+                    {
+                        Status = read["Status"].ToString(),
+                        CaseID = Convert.ToInt32(read["Case_ID"])
+                    };
+                }
+            }
+        }
+
+        protected void Report5_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Date_Of_Birth, Status, a.Case_ID FROM RJL_CASE a JOIN CASE_FILE b ON a.Case_ID = b.Case_ID JOIN PERSON c ON c.Person_ID = b.Person_ID JOIN OFFENDER d ON d.Person_ID = c.Person_ID ORDER BY Date_Of_Birth WHERE Date_Of_Birth = @age AND Case_ID = @CaseID AND Status = @status";
+
+            using (SqlConnection conn = new SqlConnection(Constants.DSN))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("Status", status);
+                cmd.Parameters.AddWithValue("CaseID", caseID);
+
+                SqlDataReader read = cmd.ExecuteReader();
+
+                if (read.Read())
+                {
+                    Case result = new Case()
+                    {
+                        Status = read["Status"].ToString(),
+                        CaseID = Convert.ToInt32(read["Case_ID"])
+                    };
+                }
+            }
+        }
+    }
+}
